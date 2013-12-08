a5f9aa2b0837a532383ae0e7ec9edb1537dea73a
rrrrong <rrrrong@gmail.com>
2013-12-08 15:00:23 +0800

    Update sign.rb
    support RSA sign verify

--- lib/alipay/sign.rb
+++ lib/alipay/sign.rb
@@ -1,21 +1,37 @@
+# -*- encoding : utf-8 -*-
 require 'digest/md5'
+require 'openssl'
+require 'base64'
 
 module Alipay
   module Sign
-    def self.generate(params)
+    def self.generate(params)      
       query = params.sort.map do |key, value|
         "#{key}=#{value}"
       end.join('&')
+      Digest::MD5.hexdigest("#{query}#{Alipay.key}")      
+    end
 
-      Digest::MD5.hexdigest("#{query}#{Alipay.key}")
+    def self.rsa_verify?(params,sign)
+      query = params.sort.map do |key, value|
+        "#{key}=#{value}"
+      end.join('&')
+      pub = OpenSSL::PKey::RSA.new(Alipay.ali_pub_key)      
+      result = pub.verify("sha1", Base64.decode64(sign), query.force_encoding("utf-8"))          
     end
 
     def self.verify?(params)
-      params = Utils.stringify_keys(params)
-      params.delete('sign_type')
+      params = Utils.stringify_keys(params)      
       sign = params.delete('sign')
-
-      generate(params) == sign
+      sign_type = params.delete('sign_type')
+      case sign_type        
+      when 'MD5'
+        generate(params) == sign
+      when 'RSA'
+        rsa_verify?(params,sign)
+      else
+        false
+      end      
     end
   end
 end
